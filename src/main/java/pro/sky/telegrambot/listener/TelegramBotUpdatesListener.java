70ec0b34f3282b7748970ba209ea75b019df04e3
Artur Vakhitov <arthurvakhitov@gmail.com>
2024-03-30 19:33:15 +0300

    Add sender, repository

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.service.TelegramBotSender;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -16,8 +17,16 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+
+    private final TelegramBotSender telegramBotSender;
+
+    private final String GREETING_MESSAGE = "Привет! Это бот для твоих напоминаний.";
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, TelegramBotSender telegramBotSender) {
+        this.telegramBot = telegramBot;
+        this.telegramBotSender = telegramBotSender;
+    }
 
     @PostConstruct
     public void init() {
@@ -28,7 +37,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String message = update.message().text();
+            Long chatID = update.message().chat().id();
+            if (message.equals("/start")) {
+                logger.info("Получили сообщение о запуске бота с текстом" + message);
+                telegramBotSender.send(chatID, GREETING_MESSAGE);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,79 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long chatID;
+    private String message;
+    private LocalDateTime notificationDateTime;
+
+    public NotificationTask(Long id, Long chatID, String message, LocalDateTime notificationDateTime) {
+        this.id = id;
+        this.chatID = chatID;
+        this.message = message;
+        this.notificationDateTime = notificationDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatID() {
+        return chatID;
+    }
+
+    public void setChatID(Long chatID) {
+        this.chatID = chatID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getNotificationDateTime() {
+        return notificationDateTime;
+    }
+
+    public void setNotificationDateTime(LocalDateTime notificationDateTime) {
+        this.notificationDateTime = notificationDateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(id, that.id) && Objects.equals(chatID, that.chatID) && Objects.equals(message, that.message) && Objects.equals(notificationDateTime, that.notificationDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chatID, message, notificationDateTime);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationTask{" +
+                "id=" + id +
+                ", chatID=" + chatID +
+                ", message='" + message + '\'' +
+                ", notificationDateTime=" + notificationDateTime +
+                '}';
+    }
+}
